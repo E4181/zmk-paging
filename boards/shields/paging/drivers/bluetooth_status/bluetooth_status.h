e116c3447ec1e45046b66ebba2a9455de9be1d59
E_huang <176415120+E4181@users.noreply.github.com>
2026-01-24 00:55:47 +0800

    dwq

--- boards/shields/paging/drivers/bluetooth_status/bluetooth_status.h
+++ boards/shields/paging/drivers/bluetooth_status/bluetooth_status.h
@@ -12,17 +12,8 @@
 extern "C" {
 #endif
 
-/* 设备树配置宏 */
-#define BLUETOOTH_STATUS_NODE DT_ALIAS(bluetooth_status)
-#define BLUETOOTH_STATUS_LABEL DT_LABEL(BLUETOOTH_STATUS_NODE)
-
 /* 获取设备指针的宏 */
-#define BLUETOOTH_STATUS_DEVICE DEVICE_DT_GET_OR_NULL(BLUETOOTH_STATUS_NODE)
-
-/* 公开的API函数 */
-int bluetooth_status_set_pattern(uint8_t pattern);
-int bluetooth_status_get_state(void);
-int bluetooth_status_enable(bool enable);
+#define BLUETOOTH_STATUS_DEVICE DEVICE_DT_GET_OR_NULL(DT_ALIAS(bluetooth_status_led))
 
 #ifdef __cplusplus
 }
